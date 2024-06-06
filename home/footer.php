60d6e7c42e5b774eaf54232390a94d83ec39c55d
mintchoc11 <jayshreevj5@gmail.com>
2024-06-06 17:29:48 +0530

    add files

---
+++ home/footer.php
@@ -0,0 +1,27 @@
+<!-- copyright and contactUs fixed footer -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.footer {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    
+    bottom: 0;
+    background-color: #2ab7ca;
+    color: white;
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+
+  <p style="margin-top: 4px;margin-bottom:-10px">Copyright © 2020 Designed By Snehil Jena and Shailly Agarwal</p><br>
+  <a href="mailto:quickpick2024@gmail.com" style="color:brown">ContactUs</a>
+
+</footer>
+
+</body>
+</html>
